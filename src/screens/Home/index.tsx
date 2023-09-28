abd70b1c7fc94ee59b775f44930934bc002d5d96
Carlos Filipe <thecarlosfilipe@gmail.com>
2023-09-27 22:42:13 -0300

    Remove ScrollView and add FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
 
@@ -36,17 +36,23 @@ export function Home() {
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {
-          participants.map(participant => (
-            <Participant
-              key={participant} 
-              name={participant} 
-              onRemove={() => handleParticipantRemove(participant)} 
-            />
-          ))
-        }
-      </ScrollView>
+      <FlatList\
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item} 
+            name={item} 
+            onRemove={() => handleParticipantRemove(item)} 
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
 
     </View>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -21,7 +21,7 @@ export const styles = StyleSheet.create({
     height: 56,
     backgroundColor: '#1f1e25',
     borderRadius: 8,
-    color: '#FFF',
+    color: '#fff',
     padding: 16,
     marginRight: 8,
   },
@@ -42,5 +42,10 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     marginTop: 36,
     marginBottom: 42,
+  },
+  listEmptyText: {
+    color: '#fff',
+    fontSize: 14,
+    textAlign: 'center',
   }
 })
